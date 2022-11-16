76ee48f9be7328dd0047693bca0c1ac31ac19b0b
sebasthub <sebastiaonogueira@unitins.br>
2022-11-16 14:41:55 -0300

    listagem de manutencoes

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,7 @@
 const express = require('express') ;
 const router = express.Router();
 const carroController = require('./controllers/carroController');
+const manutencaoController = require('./controllers/manutencaoController');
 router.get('/carros', carroController.buscarTodos);
+router.get('/manutencoes', manutencaoController.buscarTodos);
 module.exports = router;
---
+++ src/services/manutencaoService.js
@@ -0,0 +1,11 @@
+const db = require('../db')
+module.exports = {
+    buscarTodos: () => {
+        return new Promise((aceito, rejeitado) => {
+            db.query('SELECT * FROM manutencao', (error, results) => {
+                if (error) { rejeitado(error); return; } 
+                aceito(results);
+            });
+        });
+    },
+};
