31d2d3ca1f5e1ccc165a8c1999761a02cccce902
sebasthub <sebastiaonogueira@unitins.br>
2022-11-16 16:09:02 -0300

    model de manutencao criado

---
+++ src/model/Manutencao.ts
@@ -0,0 +1,11 @@
+export interface Manutencao {
+    id: number;
+    data: Date;
+    solicitante: String;
+    veiculo: String;
+    empresa: String;
+    retorno: Date;
+    valor: number;
+    problemas: String;
+    
+}
